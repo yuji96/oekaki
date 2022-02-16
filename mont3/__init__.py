e4d26ee46114a15b6a634ec54c85dae35ca46370
yuji96 <y.yama683@gmail.com>
2022-02-17 00:16:42 +0900

    enable to validate figures created with matplotlib

--- mont3/__init__.py
+++ mont3/__init__.py
@@ -2,3 +2,7 @@ from .mpl_wrapper import *  # noqa
 from .validation import *  # noqa
 import matplotlib.pyplot as plt
 import japanize_matplotlib  # noqa
+
+def show(strict=True):
+    validate(plt.gcf(), strict)
+    plt.show()
--- mpl.py
+++ mpl.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-from mont3 import validate
+import mont3
 
 x = np.linspace(0, 2 * np.pi, 50)
 
@@ -9,8 +9,7 @@ fig, ax = plt.subplots(1, 2)
 ax[0].plot(x, np.sin(x))
 ax[1].scatter(x, np.cos(x))
 ax[0].plot(x, np.cos(x))
-validate(fig)
-plt.show()
+mont3.show()
 
 # x = np.linspace(0, 4 * np.pi, 50)
 # fig, ax = plt.subplots(2, 2)
