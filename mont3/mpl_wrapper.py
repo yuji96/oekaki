5aaf869256ba181c5f98698ec4f35e6b666e72cf
yuji96 <y.yama683@gmail.com>
2022-02-16 19:36:06 +0900

    add validation

--- mont3/mpl_wrapper.py
+++ mont3/mpl_wrapper.py
@@ -1,9 +1,14 @@
 from __future__ import annotations
+import warnings
+
 from matplotlib.axes import Axes
+from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 
 
 class LazyAxes(Axes):
+
+    attrs = ["kind", "args", "kwargs", "__str__"]
 
     def __init__(self):
         self.kind: str = None
@@ -11,19 +16,15 @@ class LazyAxes(Axes):
         self.kwargs = {}
 
     def __getattribute__(self, name: str):
+        if name == "attrs" or name in self.attrs:
+            return super().__getattribute__(name)
 
         def store(*args, **kwargs):
-            self._store(name, args, kwargs)
+            self.kind = name
+            self.args = args
+            self.kwargs = kwargs
 
-        if name in dir(Axes):
-            return store
-
-        return super().__getattribute__(name)
-
-    def _store(self, kind, args, kwargs):
-        self.kind = kind
-        self.args = args
-        self.kwargs = kwargs
+        return store
 
     def __str__(self):
         # FIXME: ここが無視されてる
@@ -32,48 +33,49 @@ class LazyAxes(Axes):
 
 class figure(Axes):
 
-    def __init__(self):
-        # TODO: rename lazyaxes
-        self.graphes = []
+    def __init__(self, strict=True):
+        self.lazyaxes = []
         self.dim = None
+        self.strict = strict
 
     def __getitem__(self, key) -> LazyAxes:
         if self.dim == 0:
-            raise TypeError(
-                "Single mode is selected. In single mode, this object is not subscriptable. ex) fig.plot(...)"
-            )
+            raise TypeError("Single mode is selected."
+                            " In single mode, this object is not subscriptable."
+                            " ex) fig.plot(...)")
 
         if isinstance(key, int):
             if self.dim == 2:
-                raise ValueError(
-                    "Table mode is selected. Specify an integer sequence of length 2. ex) fig[0, 0].plot(...)"
-                )
+                raise ValueError("Table mode is selected."
+                                 " Specify an integer sequence of length 2."
+                                 " ex) fig[0, 0].plot(...)")
             elif self.dim is None:
                 self.dim = 1
 
             # for line layout
             ax = LazyAxes()
-            self.graphes.append(((0, key), ax))
+            self.lazyaxes.append(((0, key), ax))
             return ax
 
         elif isinstance(key, tuple):
             if self.dim == 1:
-                raise ValueError("Line mode is selected. Specify an integer. ex) fig[0].plot(...)")
+                raise ValueError("Line mode is selected. Specify an integer."
+                                 " ex) fig[0].plot(...)")
             elif self.dim is None:
                 self.dim = 2
 
             if len(key) != 2:
-                raise ValueError(
-                    "Specify an integer or an integer sequence of length 2.")
+                raise ValueError("Specify an integer or"
+                                 " an integer sequence of length 2.")
 
             # for table layout
             ax = LazyAxes()
-            self.graphes.append((key, ax))
+            self.lazyaxes.append((key, ax))
             return ax
 
         else:
-            raise TypeError(
-                "Specify an integer or an integer sequence of length 2.")
+            raise TypeError("Specify an integer or"
+                            " an integer sequence of length 2.")
 
     def __getattribute__(self, name):
         if name in dir(Axes):
@@ -84,12 +86,12 @@ class figure(Axes):
                     "Single mode is selected. Get axes via indices. ex) fig[0].plot(...)"
                 )
             ax = LazyAxes()
-            self.graphes.append(((0, 0), ax))
+            self.lazyaxes.append(((0, 0), ax))
             return getattr(ax, name)
         return super().__getattribute__(name)
 
-    def show(self, *args, **kwargs):
-        pos, graphes = zip(*self.graphes)
+    def show(self, filename=None, *args, **kwargs):
+        pos, graphes = zip(*self.lazyaxes)
 
         rmax, cmax = 0, 0
         for r, c in pos:
@@ -109,9 +111,35 @@ class figure(Axes):
             getattr(ax, graph.kind)(*graph.args, **graph.kwargs)
             ax.grid(True)
 
-        fig.show(*args, **kwargs)
+        validate(fig, strict=self.strict)
+
         plt.tight_layout()
-        plt.show()
+        if filename is None:
+            plt.show()
+        else:
+            plt.savefig(filename)
 
     def __str__(self):
         return f"<figure: {id(self)}>"
+
+
+class Mont3Warning(DeprecationWarning):
+    pass
+
+
+def validate(fig: Figure, strict=True):
+    if strict:
+        warnings.simplefilter('error', Mont3Warning)
+
+    check_list = ["xlabel", "ylabel"]
+
+    all_results = {}
+    for i, ax in enumerate(fig.get_axes()):
+        results = []
+        for check in check_list:
+            if not getattr(ax, f"get_{check}")():
+                results.append(check)
+        if results:
+            all_results[i] = results
+
+    warnings.warn(f"set below\n\t{str(all_results)}", Mont3Warning, stacklevel=3)
