c156abc2745dc0e4f8c09fbf844db6fe53aa5b7c
yuji96 <y.yama683@gmail.com>
2022-02-16 15:33:23 +0900

    update example.py

--- example.py
+++ example.py
@@ -2,15 +2,15 @@ import mont3 as plt
 import numpy as np
 
 x = np.linspace(0, 2 * np.pi, 50)
+
 fig = plt.figure()
-fig[0].plot(x, np.sin(x))
-fig[1].scatter(x, np.cos(x))
-fig[0].plot(x, np.cos(x))
+fig.plot(x, np.sin(x))
 fig.show()
 
-x = np.linspace(0, 4 * np.pi, 50)
 fig = plt.figure()
-fig[0, 0].plot(x, np.sin(x))
-fig[1, 1].plot(x, np.cos(x))
-fig[0, 0].plot(x, np.cos(x))
+fig[1].plot(x, np.sin(x))
+fig.show()
+
+fig = plt.figure()
+fig[1, 1].plot(x, np.sin(x))
 fig.show()
