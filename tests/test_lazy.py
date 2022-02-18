eca25d58cb77e121f26032dd2466073918f56a01
yuji96 <y.yama683@gmail.com>
2022-02-18 10:27:10 +0900

    #5 normal system tests

--- tests/test_lazy.py
+++ tests/test_lazy.py
@@ -18,14 +18,44 @@ x = np.linspace(0, 2 * np.pi, 50)
 
 @compare_figures
 def test_single():
-    expected, axes = pyplot.subplots(1, 1)
-    axes.plot(x, np.sin(x))
-    axes.grid(True)
-    axes.set(xlabel="あ", ylabel="い")
+    expected, ax = pyplot.subplots(1, 1)
+    ax.plot(x, np.sin(x))
+    ax.grid(True)
+    ax.set(xlabel="あ", ylabel="い")
 
     actual = mont3.figure(strict=False)
     actual.plot(x, np.sin(x))
     actual.set(xlabel="あ", ylabel="い")
+    actual, axes = actual._draw()
+    return expected, actual
+
+
+@compare_figures
+def test_line():
+    expected, axes = pyplot.subplots(1, 2)
+    ax = axes[1]
+    ax.plot(x, np.sin(x))
+    ax.grid(True)
+    ax.set(xlabel="あ", ylabel="い")
+
+    actual = mont3.figure(strict=False)
+    actual[1].plot(x, np.sin(x))
+    actual[1].set(xlabel="あ", ylabel="い")
+    actual, axes = actual._draw()
+    return expected, actual
+
+
+@compare_figures
+def test_table():
+    expected, axes = pyplot.subplots(2, 2)
+    ax = axes[1, 1]
+    ax.plot(x, np.sin(x))
+    ax.grid(True)
+    ax.set(xlabel="あ", ylabel="い")
+
+    actual = mont3.figure(strict=False)
+    actual[1, 1].plot(x, np.sin(x))
+    actual[1, 1].set(xlabel="あ", ylabel="い")
     actual, axes = actual._draw()
     return expected, actual
 
